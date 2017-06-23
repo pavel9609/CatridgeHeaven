3e0b4a7d116061c5fe1cbb3fb87c34fa7d419e44
romagavr <romagavr@hotmail.com>
2017-06-23 14:16:57 +0300

    new

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv)
 {
     QApplication app(argc,argv);
     qDebug()<<"Hello Git 2";
-    qDebug()<<"Skoro obed!";
+    qDebug()<<"Skoro obedcss!";
     MainWindow w;
     w.show();
     return app.exec();
