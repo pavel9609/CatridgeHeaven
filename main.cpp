060e93e057b789ede97d7ba3a9b609583f41da29
romagavr <romagavr@hotmail.com>
2017-06-23 14:34:23 +0300

    sdf

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
     QApplication app(argc,argv);
     qDebug()<<"Hello Git 2";
     qDebug()<<"Skoro obedcss!";
-    //sdsjdb
+    //sdsjdbxscscscxzxzx
     MainWindow w;
     w.show();
     return app.exec();
