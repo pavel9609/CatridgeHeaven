d2f6f4b2a0584e49d49e6fd9b89834f820757c2c
hammurapiya <varuar@yandex.ru>
2017-06-23 14:17:02 +0300

    newbd

--- dbase.cpp
+++ dbase.cpp
@@ -5,4 +5,5 @@ DBase::DBase()
     db = QSqlDatabase::addDatabase("QSQLITE","CartridgeDB");    //Добавляем базу, метод может быть только статических
     if (db.isValid()) qDebug()<<"woo-hoo";
     else qDebug()<<db.lastError().text();  //Если ошибка
+    //sdfgvhb
 }
